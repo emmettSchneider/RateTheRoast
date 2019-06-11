cfd5445bffcde3c779aec5d4f01531a7d1a0f541
Emmett Schneider <emmett.f.schneider@gmail.com>
2019-06-10 21:16:36 -0500

    completed models

--- RateTheRoast/Models/Roaster.cs
+++ RateTheRoast/Models/Roaster.cs
@@ -1,11 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RateTheRoast.Models
 {
     public class Roaster
     {
+       
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string City { get; set; }
+
+        [Required]
+        public string State { get; set; }
+
+        [Display(Name = "Roaster Logo")]
+        public string ImagePath { get; set; }
+
+
     }
 }
