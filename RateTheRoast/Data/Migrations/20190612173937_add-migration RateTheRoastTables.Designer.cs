16440bd17ef190de4493e0d287a950384713b02a
Emmett Schneider <emmett.f.schneider@gmail.com>
2019-06-12 12:44:29 -0500

    migration and database update successful

--- RateTheRoast/Data/Migrations/20190612173937_add-migration RateTheRoastTables.Designer.cs
+++ RateTheRoast/Data/Migrations/20190612173937_add-migration RateTheRoastTables.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RateTheRoast.Data;
 
 namespace RateTheRoast.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190612173937_add-migration RateTheRoastTables")]
+    partial class addmigrationRateTheRoastTables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
